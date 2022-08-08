455a8275dae302287ef8e68ba528a251f70cd01a
homebeach <petri.kotiranta@gmail.com>
2022-08-08 22:47:57 +0300

    Lisää logiikkaa apiin

--- src/main/java/com/movie/MovieServlet.java
+++ src/main/java/com/movie/MovieServlet.java
@@ -6,15 +6,16 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import com.mongodb.*;
+import com.mongodb.client.*;
+import com.mongodb.client.MongoClient;
 import org.bson.BsonDocument;
 import org.bson.BsonInt64;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoException;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
+
+import static com.mongodb.client.model.Filters.eq;
+import static java.lang.Integer.parseInt;
 
 @WebServlet("/MovieServlet")
 public class MovieServlet extends HttpServlet {
@@ -26,12 +27,47 @@ public class MovieServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
+        BasicDBList and = new BasicDBList();
+
+        String paramName = request.getParameter("name");
+        if(paramName != null) {
+             and.add(new BasicDBObject("name", paramName));
+
+        }
+        String paramYear = request.getParameter("year");
+        if(paramYear != null) {
+             and.add(new BasicDBObject("year", parseInt(paramYear)));
+
+        }
+
+        String paramGenre = request.getParameter("genres");
+        if(paramGenre != null) {
+             and.add(new BasicDBObject("genres", paramGenre));
+        }
+
+        String paramAgeLimit = request.getParameter("ageLimit");
+        if(paramAgeLimit != null) {
+            and.add(new BasicDBObject("ageLimit", paramAgeLimit));
+        }
+
+        BasicDBObject query;
+
+        if(and.size() > 0) {
+            query = new BasicDBObject("$and", and);
+        }
+        else {
+            query = new BasicDBObject();
+        }
+
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("admin");
+            MongoDatabase database = mongoClient.getDatabase("test");
             try {
-                Bson command = new BsonDocument("ping", new BsonInt64(1));
-                Document commandResult = database.runCommand(command);
-                out.println("<h3>Connected successfully to server.</h3>");
+                MongoCollection<Document> movies = database.getCollection("movies");
+                MongoCursor<Document> cursor = movies.find(query).iterator();
+                while (cursor.hasNext()) {
+                    out.println("collection is " +cursor.next().toJson() );
+                }
+
             } catch (MongoException me) {
                 out.println("<h3>An error occurred while attempting to run a command: " + me + "</h3>");
             }
